0860b3ea72f4b027a392ca1d9418542bf0e203b8
Mike Peiman <mikepeiman@gmail.com>
2020-01-08 18:00:53 -0500

    It appears I finally got a proper settings initialization checking LS. Read up on Promises a bit, that helped. Row & columns dynamically adjust gameboard; gameplay is preserved (near-instant redraw); player moves and scoring are preserved. This is a good milestone state for the app.

--- src/components/MainMenu.svelte
+++ src/components/MainMenu.svelte
@@ -24,26 +24,75 @@
     size: 24,
     gutter: 0
   };
+  $: settings, initializeSettings();
   // $: console.log(`MainMenu settings.rows: ${settings.rows}`);
   // $: console.log(`MainMenu settings.columns: ${settings.columns}`);
 
   onMount(() => {
     console.log(`MainMenu onMount(), settings`, settings);
-    storeSettings.set(settings)
+    initializeSettings();
+    storeSettings.set(settings);
     storeSettings.subscribe(value => {
       console.log(`MainMenu => storeSettings.subscribe value => `, value);
       // settings = value;
     });
-    
 
     // settings.numberOfPlayers;
   });
+
+  function initializeSettings() {
+    let initSettings = new Promise((resolve, reject) => {
+      if (!settings) {
+        let settings = {
+          numberOfPlayers: 3,
+          movesPerTurn: 8,
+          cellsToScore: 3,
+          bonusForCompleteRow: 15,
+          rows: 12,
+          columns: 20,
+          size: 24,
+          gutter: 0
+        };
+      }
+      if (settings.rows) {
+        settings = settings;
+      }
+      let lsSet = localStorage.getItem("settings");
+      if (lsSet) {
+        let lsJSet = JSON.parse(lsSet);
+        if (lsJSet) {
+          if (lsJSet.rows) {
+            console.log(
+              `initSettings :PROMISE: => localStorage settings contains rows property`
+            );
+            storeSettings.set(lsJSet);
+            resolve(lsJSet);
+          }
+        }
+      } else {
+        let reason = new Error("Initialized settings");
+        reject(reason);
+      }
+    });
+
+    initSettings
+      .then(s => {
+        console.log(
+          `Here are the settings returned from our init promise: `,
+          s
+        );
+      })
+      .catch(err => {
+        console.log(`Here is the error returned from our promise: `, err);
+      });
+  }
 
   function updatePlayers() {
     console.log(`MainMenu component, clicked to test countPoints: `, players);
   }
   function triggerGameBoardUpdate(e) {
     dispatch("updateGameSettings", settings);
+    console.log(`triggerGameBoardUpdate settings should write to LS: `, settings)
     storeSettings.set(settings);
     e.target.style.width = `${e.target.value.toString().length + 0.5}ch`;
     console.log(
