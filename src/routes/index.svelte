0de8da71a1777cb52635aa5c51a9e8ea4cb8353e
Mike Peiman <mikepeiman@gmail.com>
2020-01-10 10:05:33 -0500

    More work on homepage layout in progress

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <style lang="scss">
+  $input-blue: rgba(50, 200, 255, 1);
   h1,
   figure,
   p {
@@ -18,13 +19,27 @@
   }
 
   figure {
-    margin: 0 0 1em 0;
+    display: block;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    margin-inline-start: 0;
+    margin-inline-end: 0;
   }
 
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
+  // img {
+  //   width: 100%;
+  //   max-width: 400px;
+  //   margin: 0 0 1em 0;
+  // }
+
+  .pagelink-button {
+    padding: 1rem 2rem;
+    background: #1a1a1a;
+    border-radius: 5px;
+    text-decoration: none;
+    color: $input-blue;
+    border: 3px solid $input-blue;
+    box-shadow: 0 0 3px 3px rgba(50, 200, 255, 0.5);
   }
 
   p {
@@ -37,11 +52,38 @@
     justify-content: center;
     align-items: center;
   }
+  .figure-wrapper {
+    display: flex;
+    & img {
+      height: 40vh;
+    }
+  }
 
   .two-columns {
     display: grid;
     max-width: 80vw;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 50vw 50vw;
+    margin: 0 10vw;
+
+    // & figure {
+    //   max-width: 100%;
+    //   display: flex;
+    //   flex-direction: column;
+    //   justify-content: center;
+    //   align-items: center;
+    // }
+    & .text-content {
+      // background: #eee;
+      color: #eee;
+      padding: 2rem;
+      margin: 2rem;
+      border-radius: 10px;
+    }
+  }
+  .three-columns {
+    display: grid;
+    max-width: 80vw;
+    grid-template-columns: 1fr 1fr 1fr;
 
     & figure {
       max-width: 100%;
@@ -55,6 +97,9 @@
       padding: 2rem;
       border-radius: 10px;
     }
+  }
+  img#scoreboard {
+    width: 10vw;
   }
 
   @media (min-width: 480px) {
@@ -70,11 +115,16 @@
 <div class="homepage-wrapper">
 
   <h1>SUPER Tic Tac Toe!</h1>
+  <a
+    class="pagelink-button"
+    class:selected={segment === 'tictactoe'}
+    href="TicTacToe">
+    PLAY NOW!
+  </a>
   <div class="columns two-columns">
 
     <div class="text-content">
-      <h2>
-        Here's a fun, colorful upgrade to a classic game.</h2>
+      <h2>Here's a fun, colorful upgrade to a classic game.</h2>
       <p>
         Instead of a paltry 3x3 grid, you can now make grids of any size!
         (practical limitations of screen size apply)
@@ -82,18 +132,30 @@
       <h2>HOW TO WIN:</h2>
       <p>Score the most points! Easy, right?</p>
     </div>
-    <figure>
-      <img alt="Game Board" src="tictactoe-gameboard-5-players.png" />
-      <figcaption>The game board</figcaption>
-    </figure>
 
+    <figure class="figure-wrapper">
+      <figure>
+        <img
+          id="scoreboard"
+          alt="Scoreboard"
+          src="tictactoe-players-scoreboard.png" />
+        <figcaption>The score board</figcaption>
+      </figure>
+      <figure>
+        <img
+          id="gameboard"
+          alt="Gameboard"
+          src="tictactoe-gameboard-5-players.png" />
+        <figcaption>The game board</figcaption>
+      </figure>
+      <figure>
+        <img id="settings" alt="Settings" src="tictactoe-settings-2.png" />
+        <figcaption>Game settings</figcaption>
+      </figure>
+    </figure>
   </div>
   <div class="columns two-columns">
 
-    <figure>
-      <img alt="Borat" src="tictactoe-players-scoreboard.png" />
-      <figcaption>HIGH FIVE! (mike)</figcaption>
-    </figure>
     <div class="text-content">
       <h3>FEATURES:</h3>
       <ul>
@@ -112,7 +174,6 @@
 
   </div>
 
-  <a class:selected={segment === 'tictactoe'} href="TicTacToe">tictactoe</a>
   <p>
     <strong>
       Try editing this file (src/routes/index.svelte) to test HOT reloading.
