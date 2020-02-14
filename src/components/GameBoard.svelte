9da0a8aa4e692f8e66e0cfa92fe30b4542692e30
Mike Peiman <mikepeiman@gmail.com>
2020-02-13 17:09:58 -0800

    Lastmove function works correctly now at first use. Not tested thoroughly yet.

--- src/components/GameBoard.svelte
+++ src/components/GameBoard.svelte
@@ -237,6 +237,12 @@
     dispatch("move", e.detail);
   }
 
+  const delay = (amount = number) => {
+    return new Promise(resolve => {
+      setTimeout(resolve, amount);
+    });
+  };
+
   async function renderGameBoardReload(delayMS) {
     // console.log(
     //   `\n\nGameBoard => renderGameBoardReload called! We should see our ${settings.movesPerTurn} moves....`,
@@ -254,12 +260,6 @@
       cellSize,
       settings.gutter
     );
-
-    const delay = (amount = number) => {
-      return new Promise(resolve => {
-        setTimeout(resolve, amount);
-      });
-    };
 
     if (gameHistoryTurns) {
       if (gameHistoryTurns.length) {
@@ -364,19 +364,41 @@
     }
   }
 
-  
-  async function tickFinalTurnMoves() {
+  async function tickFinalTurnMove() {
     // loop through gameboard cells to build an array of those whose data-attribute data-ticked is false (!data-ticked)
     // run the renderGameBoardReload with argument so it does not redraw, but only draws the final moves with the
     // small animation delay (add transitions for smoother effect?)
-    console.log(`tickFinalTurnMoves rows ${rows} and columns ${columns}`)
+    console.log(`tickFinalTurnMove rows ${rows} and columns ${columns}`);
+    let unticked = [];
+    let p = currentPlayer.id;
+    let customColor = `--player-color: ${players[p].colorMain}`;
+    customMarkerSize = `--cell-mark-size: ${Math.floor(cellSize / 2)}px`;
     for (let r = 0; r < rows; r++) {
       for (let c = 0; c < columns; c++) {
-        let id = `R${r}C${c}`
-        let cell = document.getElementById(id)
-        console.log(`tickFinalTurnMoves cell id ${id}`, cell)
-        let ticked = cell.getAttribute('data-ticked')
-        console.log(`data-ticked: ${ticked}`)
+        let id = `R${r}C${c}`;
+        let cell = document.getElementById(id);
+        console.log(`tickFinalTurnMove cell id ${id}`, cell);
+        let ticked = cell.getAttribute("data-ticked");
+        console.log(`data-ticked: ${ticked}`);
+        if (ticked === "false") {
+          console.log(`!ticked! cell ${id}, calling tickThis(cell) `, cell)
+          // unticked = [...unticked, cell];
+          // cell.style = `${customColor}; ${customMarkerSize}`;
+          // cell.style.margin = settings.gutter + "px";
+          // cell.style.width = cellSize + "px";
+          // cell.style.height = cellSize + "px";
+          // cell.setAttribute("data-mark", players[p].mark);
+          // cell.setAttribute("data-ticked", true);
+          // cell.classList.add("locked", "ticked");
+          // cell.classList.remove("unticked");
+          // cell.setAttribute("locked", true);
+          // cell.style.border = "1px solid rgba(0,0,0,0.5)";
+          let delayMS = 250;
+          if (delayMS > 0) {
+            await delay(delayMS);
+          }
+          tickThis(cell)
+        }
       }
     }
   }
@@ -638,17 +660,17 @@
   }
 
   function tickThis(cell) {
-    // console.log(
-    //   `tickThis(cell) BEFORE, cellSize ${cellSize} `,
-    //   settings,
-    //   cell
-    // );
+    console.log(
+      `tickThis(cell) BEFORE, cellSize ${cellSize} `,
+      settings,
+      cell
+    );
     dispatch("moveNotification", cell);
-    // console.log(
-    //   "tickThis(cell) players, currentPlayer",
-    //   players,
-    //   currentPlayer
-    // );
+    console.log(
+      "tickThis(cell) players, currentPlayer",
+      players,
+      currentPlayer
+    );
     let id = cell.id;
     let row = id[1];
     let column = id[3];
@@ -909,9 +931,10 @@
     console.log(
       `checkForLastMove value (turns remaining): ${checkForLastMove}`
     );
-    tickFinalTurnMoves()
+    // tickFinalTurnMove();
     if (checkForLastMove === 1) {
       console.log(`checkForLastMove says FINAL MOVE!!!!`);
+      tickFinalTurnMove()
     }
   }
 </script>
